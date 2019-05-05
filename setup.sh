ed5c83f1ae439bec8298d624d40beea5553eb8a5
Geelon So <geelon@uchicago.edu>
2019-05-05 15:36:37 -0400

    updated script

--- setup.sh
+++ setup.sh
@@ -2,18 +2,22 @@
 
 CURR_PATH=$(pwd)
 # download boost
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
-tar --bzip2 -xf boost_1_69_0.tar.bz2
+if [ ! -d boost_16_0 ]; then
+    wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
+    tar --bzip2 -xf boost_1_69_0.tar.bz2
+fi
 BOOST_PATH=$CURR_PATH/boost_1_69_0
 
 # install he-transformer
-git clone https://github.com/NervanaSystems/he-transformer.git
+if [ ! -d "he-transformer"]; then
+    git clone https://github.com/NervanaSystems/he-transformer.git
+fi
 
 cd he-transformer
 export HE_TRANSFORMER=$(pwd)
 mkdir build
 cd $HE_TRANSFORMER/build
-cmake .. --DBOOST_ROOT=$BOOST_PATH
+cmake .. -DBOOST_ROOT=$BOOST_PATH
 make install
 source external/venv-tf-py3/bin/activate
 
