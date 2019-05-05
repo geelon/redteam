3411dceb76f5ea7f72a4d19ec152d5c20751d2b1
Geelon So <geelon@uchicago.edu>
2019-05-05 15:37:40 -0400

    updated script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CURR_PATH=$(pwd)
 # download boost
-if [ ! -d boost_16_0 ]; then
+if [ ! -d boost_1_69_0 ]; then
     wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
     tar --bzip2 -xf boost_1_69_0.tar.bz2
 fi
