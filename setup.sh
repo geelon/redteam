0da8b7feaea549f4d5cb5df7643b1b943dbf3307
Geelon So <geelon@uchicago.edu>
2019-05-05 15:51:37 -0400

    update

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 CURR_PATH=$(pwd)
 # download boost
-if [ ! -d boost_1_69_0 ]; then
+if [ ! -d "boost_1_69_0" ]; then
     wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2
     tar --bzip2 -xf boost_1_69_0.tar.bz2
 fi
 BOOST_PATH=$CURR_PATH/boost_1_69_0
 
 # install he-transformer
-if [ ! -d "he-transformer"]; then
+if [ ! -d "he-transformer" ]; then
     git clone https://github.com/NervanaSystems/he-transformer.git
 fi
 
